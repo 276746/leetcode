24533369bbcce196daf6da37d4e343cb7ca90617
Oscar Duong <oscar.duong284@gmail.com>
2026-09-07 09:32:02 +0200

    pb 35

---
+++ src/main/java/leetcode/problem0035_count_good_nodes_in_binary_tree/Solution.java
@@ -0,0 +1,27 @@
+package main.java.leetcode.problem0035_count_good_nodes_in_binary_tree;
+
+public class Solution {
+    public int goodNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        return rec(root, root.val);
+    }
+
+    private int rec(TreeNode node, int max) {
+        if (node == null) return 0;
+
+        int acc = 0;
+
+        if (node.val >= max) {
+            max = node.val;
+            ++acc;
+        }
+
+        acc += rec(node.left, max);
+        acc += rec(node.right, max);
+
+        return acc;
+    }
+
+    public static void main(String[] args) {}
+}
